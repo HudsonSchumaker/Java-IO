d5b3a064b49cf2dbb89256f1b0c340406fed7e70
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-08-16 19:13:45 +0200

    Update TesteEscritaFileWriter.java

--- java-io/src/br/com/schumaker/io/TesteEscritaFileWriter.java
+++ java-io/src/br/com/schumaker/io/TesteEscritaFileWriter.java
@@ -8,7 +8,7 @@ public class TesteEscritaFileWriter {
 
 	public static void main(String[] args) throws IOException {
 		
-		//Fluxo de Entrada com Arquivo
+		// Fluxo de Entrada com Arquivo
 //		OutputStream fos = new FileOutputStream("lorem2.txt");
 //		Writer osw = new OutputStreamWriter(fos);
 //		BufferedWriter bw = new BufferedWriter(osw);
