7cf76106fce753034f6b7b4d5c2be36e4793f9a2
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-08-16 19:12:26 +0200

    Update TesteSerializacao.java

--- java-io/src/br/com/schumaker/io/TesteSerializacao.java
+++ java-io/src/br/com/schumaker/io/TesteSerializacao.java
@@ -8,7 +8,7 @@ public class TesteSerializacao {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-//		String nome = "Nico Steppat";
+//		String nome = "Daniel Senna";
 //		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("objeto.bin"));
 //		oos.writeObject(nome);
 //		oos.close();
